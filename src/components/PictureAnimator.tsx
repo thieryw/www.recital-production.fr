aa94eef5701aa96c4f560a4c9e9b7faec4224139
thieryw <39378411+thieryw@users.noreply.github.com>
2024-09-17 13:25:19 +0000

    update

---
+++ src/components/PictureAnimator.tsx
@@ -0,0 +1,119 @@
+import { memo, useEffect, useState } from "react";
+import { tss } from "tss";
+import { useInView } from "react-intersection-observer";
+
+
+export type PictureAnimatorProps = {
+    className?: string;
+    classes?: Partial<ReturnType<typeof useStyles>["classes"]>;
+    src: string;
+    sources?: {srcSet: string; type: string}[];
+    alt?: string;
+    animationDelay?: number;
+    width: number;
+    height: number;
+    borderRadius: string;
+}
+
+export const PictureAnimator = memo((props: PictureAnimatorProps) => {
+    const { className, src, sources, alt, animationDelay = 0, width, height, borderRadius } = props;
+    const [ref, inView] = useInView({ "triggerOnce": true, "threshold": 0.4 })
+    const [isImageVisible, setIsImageVisible] = useState(inView);
+
+
+    useEffect(() => {
+        if (inView) {
+            setIsImageVisible(true)
+        }
+
+    }, [inView])
+
+    const { classes, cx } = useStyles({
+        "inView": isImageVisible,
+        "classesOverrides": props.classes,
+        animationDelay,
+        width,
+        height,
+        borderRadius
+    });
+
+    return <div ref={ref} className={cx(classes.root, className)}>
+
+        <div className={classes.animatedDiv}>
+            <picture>
+                {
+                    sources?.map((source, index) => <source key={index} {...source} />)
+                }
+                <img className={classes.image} /*ref={imageRef}*/ src={src} alt={alt} />
+
+            </picture>
+
+        </div>
+    </div>
+
+})
+
+const useStyles = tss.withParams<
+    {
+        inView: boolean;
+        animationDelay: number;
+        width: number;
+        height: number;
+        borderRadius: string;
+    }
+>().create(({ inView, animationDelay, borderRadius, height: _height, width: _width, windowInnerWidth, theme }) => {
+
+    const width = windowInnerWidth < theme.breakpoints.values.sm ? "100vw" : _width;
+    const height = windowInnerWidth < theme.breakpoints.values.sm ? `${_height / (_width / 100)}vw` : _height;
+
+
+    return ({
+        "root": {
+            "position": "relative",
+
+            width,
+            height,
+            borderRadius,
+            "overflow": "hidden",
+            "maxWidth": "100%",
+            ...(() => {
+                if (typeof width !== "number" || typeof height !== "number") {
+                    return;
+
+                }
+                return {
+                    "minHeight": `${height / width}vw`,
+
+                }
+            })(),
+        },
+        "animatedDiv": {
+
+            "position": "absolute",
+            "whiteSpace": "nowrap",
+            "width": inView ? width : 0,
+            "transition": "width 1000ms",
+            "transitionDelay": `${animationDelay}ms`,
+            "overflow": "hidden",
+            height
+        },
+        "image": {
+            "transform": `scale(${inView ? 1 : 1.2})`,
+            "transition": "transform 1000ms",
+            "transitionDelay": `${animationDelay}ms`,
+            "width": width,
+            "height": height,
+            ...(() => {
+                if (typeof width !== "number" || typeof height !== "number") {
+                    return;
+
+                }
+                return {
+                    "minHeight": `${height / width}vw`,
+
+                }
+            })(),
+            "objectFit": "cover"
+        }
+    })
+})
