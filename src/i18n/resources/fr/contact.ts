9b85a26428e629b15eb9d6e8b83ed003af25be75
thieryw <39378411+thieryw@users.noreply.github.com>
2025-01-03 21:08:58 +0000

    update

--- src/i18n/resources/fr/contact.ts
+++ src/i18n/resources/fr/contact.ts
@@ -4,7 +4,7 @@ export const translation: TranslationContact<"fr"> = {
     "Contact": {
         "collaborationText": "Nous serions heureux de collaborer avec vous.",
         "contactTitle": "Contact",
-        "email": "contact@recital-production.fr",
+        "email": "recitalproduction@gmail.com",
         "contactText": "Région bordelaise, Gironde, et tout le Sud-Ouest de la France.",
         "referenceSite": "Mariage.net",
         "reviewText": "Lire nos avis à"
